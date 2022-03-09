180592e2b2e12234626749b6e1062e3e88b71763
Naman Kumar Baderia <68146921+namankumar2010@users.noreply.github.com>
2022-03-09 21:02:00 +0530

    Update Header.jsx

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,7 +4,7 @@ export default function Header() {
   return (
     <div className="header">
       <div className="headerTitles">
-        <span className="headerTitleSm">React App</span>
+        <span className="headerTitleSm">React App.</span>
         <span className="headerTitleLg">BLOG</span>
       </div>
       <img
